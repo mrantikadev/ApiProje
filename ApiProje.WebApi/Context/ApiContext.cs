c0e0386d777b08b350a670f9cf76a6c44877a8eb
mrantika <94685047+mrantikadev@users.noreply.github.com>
2025-02-15 12:30:17 +0300

    Added context class and applied migrations

---
+++ ApiProje.WebApi/Context/ApiContext.cs
@@ -0,0 +1,31 @@
+﻿using ApiProje.WebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProje.WebApi.Context
+{
+    public class ApiContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApiContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Chef> Chefs { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
