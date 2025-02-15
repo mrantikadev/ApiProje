37c76db5522e58dcd3a6e430e56844295a7667ae
mrantika <94685047+mrantikadev@users.noreply.github.com>
2025-02-15 13:02:53 +0300

    Added Chef Controller

---
+++ ApiProje.WebApi/Controllers/ChefsController.cs
@@ -0,0 +1,57 @@
+﻿using ApiProje.WebApi.Context;
+using ApiProje.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProje.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChefsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ChefsController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult ChefList()
+        {
+            var values = _context.Chefs.ToList();
+            return Ok(values);
+        }
+
+        [HttpGet("GetChefById")]
+        public IActionResult GetChefById(int id)
+        {
+            return Ok(_context.Chefs.Find(id));
+        }
+
+        [HttpPost]
+        public IActionResult CreateChef(Chef chef)
+        {
+            _context.Chefs.Add(chef);
+            _context.SaveChanges();
+            return Ok("Sef sisteme basariyla eklendi");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteChef(int id)
+        {
+            var value = _context.Chefs.Find(id);
+            _context.Chefs.Remove(value);
+            _context.SaveChanges();
+            return Ok("Sef basariyla sistemden silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateChef(Chef chef)
+        {
+            _context.Chefs.Update(chef);
+            _context.SaveChanges();
+            return Ok("Sef basariyla guncellendi");
+        }
+    }
+}
